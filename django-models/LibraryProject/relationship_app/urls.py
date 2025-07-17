aa8340dcf6c099f5fa16bfffa4f0862fdef5e635
Mistire <mdiwithgod@gmail.com>
2025-07-17 15:20:38 +0300

    Implement Djagno views and url configuration..5

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from views import list_books_with_author, BookList, LibraryDetail
 
 urlpatterns = [
-  path('books_list_func/', views.list_book_with_author, name='books-list-c'),
+  path('books_list_func/', list_books_with_author, name='books-list-c'),
   path('book_list_class/', views.BookListView.as_view(), name='book-list-c'),
   path('library_detail/', views.LibraryDetailView.as_view(), name='library-detail'),
 ]
