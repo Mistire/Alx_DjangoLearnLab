2e8d25b6e49e39cd12dea68d3079b65824b36973
Mistire <mdiwithgod@gmail.com>
2025-08-03 21:37:16 +0300

    Add an api endpoint for listing books

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from
+
 
 # Create your models here.
-class Book:
+class Book(models.Model):
   title = models.CharField(max_length=100)
   author = models.CharField(max_length=100)
   publication_date = models.DateField()
