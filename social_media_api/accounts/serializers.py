6611eb5fd43f75ba974b7013b32a7686f808d186
Mistire <mdiwithgod@gmail.com>
2025-08-24 19:38:37 +0300

    Project Setup and User Authentication for a Social Media API..2

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -16,6 +16,11 @@ class RegisterSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        
+        user = get_user_model().objects.create_user(
+            username=validated_data["username"],
+            email=validated_data.get("email"),
+            password=validated_data["password"],
+        )
         Token.objects.create(user=user)
         return user
